38611785c4d915f1d2d8f6c6a9f9a89d3afafd38
Eli <elii.villa13@gmail.com>
2023-02-16 23:29:37 -0800

    new changes in the form

--- script.js
+++ script.js
@@ -203,3 +203,13 @@ particlesJS("particles-js", {
 });
 
 
+/*CSS301*/
+// matthais example
+document.querySelector("#survey-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const data = new FormData(e.target);
+  const arr = [...data];
+  console.table(arr);
+  console.log(data.getAll("langs"));
+  console.dir(Object.fromEntries(arr));
+});
